c284ba2a3e83f11dca660e511abd4c7c2db2b490
Princeton Gomes <princeton4d@gmail.com>
2020-11-06 16:24:15 +0530

    Add instance folder

--- loanapp/__init__.py
+++ loanapp/__init__.py
@@ -1,15 +1,18 @@
-from os import environ, path
+from os import environ, path, makedirs
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
-app = Flask(__name__)
+app = Flask(__name__, instance_relative_config=True)
 
-basedir = path.abspath(path.dirname(__file__))
+try:
+    makedirs(app.instance_path)
+except OSError:
+    pass
 
 app.config.from_mapping(
     SQLALCHEMY_DATABASE_URI = environ.get('DATABASE_URL') or \
-        'sqlite:///' + path.join(basedir, 'data.db'),
+        'sqlite:///' + path.join(app.instance_path, 'data.db'),
     SQLALCHEMY_TRACK_MODIFICATIONS = False,
     SECRET_KEY = 'dev'
 )
